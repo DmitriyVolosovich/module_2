3dac7557abd89e30763bbf5f98fa9d996b35baed
DmitryVolosovich <tigidinsky@gmail.com>
2024-09-14 19:28:53 +0200

    14.09.2024

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,12 @@
-first=int(input('Введите первое число: '))
-second=int(input('Введите второе число: '))
-third=int(input('Введите третье число: '))
-if first==second and first==third:
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second and first == third:
     print('Вы ввели 3 одинаковых числа')
-elif first==second or first==third or second==third:
+elif(
+        first == second
+     or first == third
+     or second == third):
     print('Вы ввели 2 одинаковых числа')
 else:
     print('нет одинаковых чисел')
