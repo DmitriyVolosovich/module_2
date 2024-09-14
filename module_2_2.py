cfd3cc31cf1720831c68302673bf0bd3183797c9
DmitryVolosovich <tigidinsky@gmail.com>
2024-09-14 15:03:12 +0200

    14.09.2024

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first=int(input('Введите первое число: '))
+second=int(input('Введите второе число: '))
+third=int(input('Введите третье число: '))
+if first==second and first==third:
+    print('Вы ввели 3 одинаковых числа')
+elif first==second or first==third or second==third:
+    print('Вы ввели 2 одинаковых числа')
+else:
+    print('нет одинаковых чисел')
