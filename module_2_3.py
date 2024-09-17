cc19c7c09a752844e3a5a8476ab0e311efdfbe5c
DmitryVolosovich <tigidinsky@gmail.com>
2024-09-17 13:28:47 +0200

    17.09.2024

---
+++ module_2_3.py
@@ -0,0 +1,19 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index_count = 0
+while len(my_list) >= index_count:
+    if my_list[index_count] < 0:
+        break
+    elif my_list[index_count] != 0:
+        print(my_list[index_count])
+    index_count += 1
+
+
+
+
+
+
+
+
+
+
